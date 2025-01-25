6eb3482bdc565eb1b7ca477ccbc35746dc5c9017
Muhammad-Devel <m.jumaboyev@mail.ru>
2025-01-25 23:08:15 +0500

    implement addTodo function to append new todos to the JSON file

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,3 +1,15 @@
-exports.addTodo = ()=>{
-    
-}
+const fs = require("fs");
+const path = require("path");
+const todosFilePath = path.join(__dirname, "../data/todos.json");
+
+exports.addTodo = (todo) => {
+  fs.readFile(todosFilePath, (err, data) => {
+    if (err) throw err;
+    let todos = JSON.parse(data);
+    todos.push(todo);
+    fs.writeFile(todosFilePath, JSON.stringify(todos), (err) => {
+      if (err) throw err;
+      console.log("Todo added successfully");
+    });
+  });
+};
