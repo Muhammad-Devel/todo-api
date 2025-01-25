2c059eee0a68bfe9b447fad3877f81f024d11b5d
Muhammad-Devel <m.jumaboyev@mail.ru>
2025-01-25 17:42:32 +0500

    create function

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,3 +1,16 @@
-exports.addTodo = ()=>{
-    
+const fs = require('fs');
+
+exports.addTodo = (todos)=>{
+    fs.existsSync('todos.json',function (exists) {
+        if (exists) {
+            console.log('yes file');
+            fs.readFile('todos.json', function readFileCallBack(err,data){
+                if (err) {
+                    console.log(err);
+                }else{
+                    
+                }
+            })
+        }
+    })
 }
