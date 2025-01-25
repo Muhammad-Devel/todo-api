494251d2145f92e2dc85617de07e2d0c4ac53931
Muhammad-Devel <m.jumaboyev@mail.ru>
2025-01-25 15:42:38 +0500

    create todo api

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+// listening the port
+app.listen(PORT, ()=>{
+    console.log(`Server ${PORT}da ishlamoqda...`);    
+})
